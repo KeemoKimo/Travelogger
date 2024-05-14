d1b55ed947d16b91812cd61fe82d99dacd46b175
Keemo Kimo <thebunthong@gmail.com>
2024-05-14 08:17:53 -0300

    Set up password text box
    Password textbox content hidden, only showed on button press

--- JS/login_page.js
+++ JS/login_page.js
@@ -1,1 +1,11 @@
-let 
+var imgEye = document.getElementById("imgEyeContent")
+var txtPass = document.getElementById("txtPassword")
+imgEye.onclick = function(){
+    if(txtPass.type == "password"){
+        imgEye.src = "file:///C:/xampp/htdocs/Travelogger/Images/EyeClose.png"
+        txtPass.type = "text"
+    }else{
+        imgEye.src = "file:///C:/xampp/htdocs/Travelogger/Images/eye.png"
+        txtPass.type = "password"
+    }
+}
