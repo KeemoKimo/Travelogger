cd0dc9472dea985d956c1afefc080d467b961c5f
Keemo Kimo <thebunthong@gmail.com>
2024-05-28 11:00:10 -0300

    fix

--- FrontEnds/addPost.php
+++ FrontEnds/addPost.php
@@ -20,55 +20,61 @@ $conn = mysqli_connect($servername, $username, $password, $database);
     <center>
         <div id="mainUploadDiv">
             <h1 id="lblUpload_mainUploadDiv">:+ upload a new memory +:</h1>
-                    <div id="imgContainer">
-                        <div id="imgContainer_images">
-                            <img src="../Images/cam1.jpg" alt="" id="img_1">
-                            <img src="../Images/china1.jpg" alt="" id="img_2">
-                            <img src="../Images/thai1.jpg" alt="" id="img_3">
-                        </div>
-                        <div id="moreImgContainer">
-                            <h1>+</h1>
-                        </div>
+            <form action="upload.php" method="post" enctype="multipart/form-data">
+                Select Image Files to Upload:
+                <input type="file" name="files[]" multiple>
+                <br>
+                <input type="submit" name="submit" value="UPLOAD">
+            </form>
+            <div id="imgContainer">
+                <div id="imgContainer_images">
+                    <img src="../Images/emptyImage.jpg" alt="" id="img_1">
+                    <img src="../Images/emptyImage.jpg" alt="" id="img_2">
+                    <img src="../Images/emptyImage.jpg" alt="" id="img_3">
+                </div>
+                <div id="moreImgContainer">
+                    <h1>+</h1>
+                </div>
+            </div>
+            <div id="locationContainer">
+                <div id="locationContainer_img">
+                    <div style="display:flex;">
+                        <img src="../Images/location.png" alt="" id="imgLocation">
+                        <h4 style="font-size: 20px;">Location : </h4>
                     </div>
-                    <div id="locationContainer">
-                        <div id="locationContainer_img">
-                            <div style="display:flex;">
-                                <img src="../Images/location.png" alt="" id="imgLocation">
-                                <h4 style="font-size: 20px;">Location : </h4>
-                            </div>
-                        </div>
-                        <div id="locationContainer_desc">
-                            <input type="text" id="txtLocation" placeholder="format : areaName, countryName">
-                        </div>
+                </div>
+                <div id="locationContainer_desc">
+                    <input type="text" id="txtLocation" placeholder="format : areaName, countryName">
+                </div>
+            </div>
+            <div id="travelDateContainer">
+                <div id="travelDateContainer_img">
+                    <div style="display:flex;">
+                        <img src="../Images/calendar.png" alt="" id="imgCalendar">
+                        <h4 style="font-size: 20px;">Travel Date : </h4>
                     </div>
-                    <div id="travelDateContainer">
-                        <div id="travelDateContainer_img">
-                            <div style="display:flex;">
-                                <img src="../Images/calendar.png" alt="" id="imgCalendar">
-                                <h4 style="font-size: 20px;">Travel Date : </h4>
-                            </div>
-                        </div>
-                        <div id="travelDateContainer_desc">
-                            <input type="date" id="dateStart">
-                            <p
-                                style="margin-left: 30px;margin-right: 30px;font-weight: lighter;margin-top: 25px;font-size: 19px">
-                                to </p>
-                            <input type="date" id="dateEnd">
-                        </div>
-                    </div>
-                    <div id="writeDescriptionContainer">
-                        <textarea name="txtLongDescription" id="txtLongDescription"
-                            placeholder="Enter a description for this memory..."></textarea>
-                    </div>
-                    <div id="lblHightlight_container">
-                        <h2 id="lblCancel"><b>
-                                < </b> cancel</h2>
-                        <h2 id="lblUpload">upload memory<b> > </b></h2>
-                    </div>
+                </div>
+                <div id="travelDateContainer_desc">
+                    <input type="date" id="dateStart">
+                    <p
+                        style="margin-left: 30px;margin-right: 30px;font-weight: lighter;margin-top: 25px;font-size: 19px">
+                        to </p>
+                    <input type="date" id="dateEnd">
+                </div>
+            </div>
+            <div id="writeDescriptionContainer">
+                <textarea name="txtLongDescription" id="txtLongDescription"
+                    placeholder="Enter a description for this memory..."></textarea>
+            </div>
+            <div id="lblHightlight_container">
+                <h2 id="lblCancel"><b>
+                        < </b> cancel memory</h2>
+                <h2 id="lblUpload">upload memory<b> > </b></h2>
+            </div>
         </div>
     </center>
     <script>
-        document.getElementById("lblCancel").onclick =function(){
+        document.getElementById("lblCancel").onclick = function () {
             document.location.href = 'http://localhost/Travelogger/FrontEnds/mainPage.php'
         }
     </script>
