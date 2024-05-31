0ee56db979c3afb7c97a0e225e71645f1d790739
Keemo Kimo <thebunthong@gmail.com>
2024-05-31 17:53:10 -0300

    Linked finished UI to detailed page
    Successfully passed data along to the next page with the use of PHP GET variable as well as redesinging the memory overview page using A tage that can support the data passing, and use a div as it's child instead.

--- FrontEnds/allMemories.php
+++ FrontEnds/allMemories.php
@@ -34,59 +34,43 @@ include ("../phpOperations/OPEN_CONN.php");
     <center>
         <div id="dividerLine"></div>
     </center>
-    <?php 
-        $userEmail = $_SESSION['userEmail'];
-        $sql = mysqli_query($conn, "select * from tbltrips where user = '$userEmail' order by dateEnd desc");
-        $num = mysqli_num_rows($sql);
-        if($num > 0){
-            while($result = mysqli_fetch_array($sql)){
-                echo "hello " .$result["location"] ." , " .$result['tripID'];
-                echo "<a href='memoryOverview.php?id=".$result['tripID']."'>No</a>";
-                echo "</br>";
-            }
-        }
-    ?>
     <?php
     $userEmail = $_SESSION['userEmail'];
     $sql = mysqli_query($conn, "select * from tbltrips where user = '$userEmail' order by dateEnd desc");
-    $rows = mysqli_fetch_all($sql, MYSQLI_ASSOC);
     $num = mysqli_num_rows($sql);
-    for($x = 0; $x < $num ; $x++){
-        ?>
-        <!-- <center>
-            <div class="tripOverviewContainer">
-                <div id="tripOverviewContainer_imgCont">
-                    <?php $_SESSION['coverImg'] = $rows[$x]['coverImg']; ?>
-                    <img src="<?php echo $rows[$x]['coverImg'] ?>" alt="pic" style="width:100%" id="imgCont">
-                </div>
-                <p style="display:none"><?php echo $rows[$x]['tripID'] ?></p>
-                <?php $_SESSION['location'] = $rows[$x]['location']; ?>
-                <h3 style="text-align:center;" id="lblLocation"><b><?php echo $rows[$x]['location'] ?></b></h3>
-                <div id="lblTravelDates_Container">
-                    <?php $_SESSION['dateStart'] = $rows[$x]['dateStart']; ?>
-                    <h3><b><?php echo $rows[$x]['dateStart'] ?></b>
-                    </h3>
-                    <h3 style="margin-left: 30px;margin-right: 30px">to</h3>
-                    <?php $_SESSION['dateEnd'] = $rows[$x]['dateEnd']; ?>
-                    <h3> <?php echo $rows[$x]['dateEnd'] ?></h3>
-                </div>
-                <p id="lblDesc">
-                    <?php $_SESSION['description'] = $rows[$x]['description']; ?>
-                    <?php echo $rows[$x]['description'] ?>
-                </p>
-            </div>
-        </center> -->
+    if ($num > 0) {
+        while ($result = mysqli_fetch_array($sql)) {
+            //echo "hello " . $result["location"] . " , " . $result['tripID'];
+            //echo "<a href='memoryOverview.php?id=" . $result['tripID'] . "' id='aFull'>No</a>";
+            echo '<center>
+                    <a href="memoryOverview.php?id=' . $result["tripID"] . '" id="aFull">
+                        <div class="insideContainer"> 
+                            <div class="insideContainer_imgCont">   
+                                <img src="'.$result['coverImg'].'" width="100%">
+                            </div>   
+                            <p style="display:none">'.$result['tripID'].'</p>
+                            <h3 style="text-align:center;" id="lblLocation"><b>'.$result['location'].'</b></h3>
+                            <div id="lblTravelDates_Container">
+                                <h3><b>'.$result['dateStart'].'</b>
+                                </h3>
+                                <h3 style="margin-left: 30px;margin-right: 30px">to</h3>
+                                <h3><b>'.$result['dateEnd'].'</b></h3>
+                            </div>
+                            <p id="lblDesc">
+                                '.$result['description'].'
+                            </p>
+                        </div>
+                    </a>
+                </center>
+                <center>
+                    <div id="dividerLine"></div>
+                </center>
+                ';
+            echo "</br>";
 
-        <center>
-            <div id="dividerLine"></div>
-        </center>
-    <?php
-        // echo '<script language="javascript">';
-        // echo 'document.getElementsByClassName("tripOverviewContainer").onclick =function(){';
-        // echo "console.log($rows[$x]['location'])";
-        // echo "}";
-        // echo '</script>';
-    } ?>
+        }
+    }
+    ?>
 </body>
 
 <!-- <script>
